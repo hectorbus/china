3f1c9a21a610a19c66efd68a590e58e600692133
Hector Bustillos <hectorbus@Hectors-MacBook-Pro.local>
2018-02-12 13:29:22 -0700

    Trumps size, count added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
-  var ini = 0, count = 0
-  $(window).on('click', function(){
-    if (ini == 0) {
-      ini = 1
+  count = 0
+  $("a, input").mousedown(function(e) {
+       e.stopPropagation();
+  });
+  $(document).mousedown(function(){
+    if (count == 0) {
       $('h1').hide()
+      $('.count').fadeIn().text((count + 1) + " china")
+    }else {
+      $('.count').text((count + 1) + " chinas")
     }
     count++
     random_image = Math.floor(Math.random()*(16)+1)
     random_sound = Math.floor(Math.random()*(19)+1)
-    audio = new Audio(`resources/chinaSounds/China_${random_sound}.mp3`);
-    pos_x = event.clientX - 100;
-    pos_y = event.clientY - 100;
+    audio = new Audio(`resources/chinaSounds/China_${random_sound}.mp3`)
+    size_val = $('.slider').val()
+    pos_x = event.clientX - (size_val / 2)
+    pos_y = event.clientY - (size_val / 2)
     $('body').prepend(`<img class='trumps_face'
                                   src='resources/trumpFaces/trump_${random_image}.png'
-                                  style='z-index:${count};top:${pos_y}px;left:${pos_x}px;'/>`)
+                                  style='width:${size_val}px;z-index:${count};top:${pos_y}px;left:${pos_x}px;'/>`)
     audio.play()
   })
 })
