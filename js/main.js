a9248004d9ac5f776a144ec1f0329c8619665968
RamiroSolis <ramirogameboy@hotmail.com>
2019-10-04 14:41:06 -0600

    Adding let's

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  count = 0
+  let count = 0
   $("a, input").mousedown(function(e) {
        e.stopPropagation();
   });
@@ -11,12 +11,12 @@ $(document).ready(function(){
       $('.count').text((count + 1) + " chinas")
     }
     count++
-    random_image = Math.floor(Math.random()*(16)+1)
-    random_sound = Math.floor(Math.random()*(19)+1)
-    audio = new Audio(`resources/chinaSounds/China_${random_sound}.mp3`)
-    size_val = $('.slider').val()
-    pos_x = event.clientX - (size_val / 2)
-    pos_y = event.clientY - (size_val / 2)
+    let random_image = Math.floor(Math.random()*(16)+1)
+    let random_sound = Math.floor(Math.random()*(19)+1)
+    let audio = new Audio(`resources/chinaSounds/China_${random_sound}.mp3`)
+    let size_val = $('.slider').val()
+    let pos_x = event.clientX - (size_val / 2)
+    let os_y = event.clientY - (size_val / 2)
     $('body').prepend(`<img class='trumps_face'
                                   src='resources/trumpFaces/trump_${random_image}.png'
                                   style='width:${size_val}px;z-index:${count};top:${pos_y}px;left:${pos_x}px;'/>`)
